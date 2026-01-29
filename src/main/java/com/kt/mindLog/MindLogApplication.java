b3bdfa02958a605e870ff7d2426cd96aa36dcaed
munyeong0103 <skymy02@ajou.ac.kr>
2026-01-29 16:29:36 +0900

    chore: project init

---
+++ src/main/java/com/kt/mindLog/MindLogApplication.java
@@ -0,0 +1,13 @@
+package com.kt.mindLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindLogApplication.class, args);
+	}
+
+}
