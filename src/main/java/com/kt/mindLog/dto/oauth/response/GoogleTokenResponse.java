ea9fb1b1750c87da0de663498d33f9c4220f1b43
jimin <jeemin065@gmail.com>
2026-03-10 16:39:13 +0900

    feat: implement DTO related Google login and config file

--- src/main/java/com/kt/mindLog/dto/oauth/response/GoogleTokenResponse.java
+++ src/main/java/com/kt/mindLog/dto/oauth/response/GoogleTokenResponse.java
@@ -1,4 +1,26 @@
 package com.kt.mindLog.dto.oauth.response;
 
-public record GoogleTokenResponse() {
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record GoogleTokenResponse(
+	@JsonProperty("token_type")
+	String tokenType,
+
+	@JsonProperty("access_token")
+	String accessToken,
+
+	@JsonProperty("id_token")
+	String idToken,
+
+	@JsonProperty("expires_in")
+	Integer expiresIn,
+
+	@JsonProperty("refresh_token")
+	String refreshToken,
+
+	@JsonProperty("scope")
+	String scope
+) {
 }
