64e56549fb6a7e08f428fed5478f20fae40625eb
munyeong0103 <skymy02@ajou.ac.kr>
2026-03-13 14:43:23 +0900

    fix: change message type (#14)

--- src/main/java/com/kt/mindLog/service/redis/RedisService.java
+++ src/main/java/com/kt/mindLog/service/redis/RedisService.java
@@ -1,42 +1,40 @@
 package com.kt.mindLog.service.redis;
+
 import java.util.List;
 
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import com.kt.mindLog.domain.enums.Role;
+import com.kt.mindLog.domain.user.Role;
 import com.kt.mindLog.dto.redis.RedisMessageRequest;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import tools.jackson.databind.ObjectMapper;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisService {
 
 	private final RedisTemplate<String, Object> redisTemplate;
+	private final ObjectMapper objectMapper;
 
 	private static final String SESSION_PREFIX = "session:";
 	private static final String MESSAGE_SUFFIX = ":messages";
 
-	/**
-	 * session message 저장
-	 */
 	public int pushMessage(String sessionId, Role role, String content) {
-
 		String key = buildKey(sessionId);
 
 		RedisMessageRequest message = new RedisMessageRequest(role.toString().toLowerCase(), content);
+		log.info("success to push message: sessionId:{},content:{}", sessionId, content);
 
 		return executeOperation(() ->
-			redisTemplate.opsForList().rightPush(key, message)
+			redisTemplate.opsForList().rightPush(key, objectMapper.writeValueAsString(message))
 		);
 	}
 
-	/**
-	 * session message 조회
-	 */
 	public List<RedisMessageRequest> getMessages(String sessionId) {
-
 		String key = buildKey(sessionId);
 
 		List<Object> list = redisTemplate.opsForList().range(key, 0, -1);
@@ -44,26 +42,20 @@ public class RedisService {
 		if (list == null) return List.of();
 
 		return list.stream()
-			.map(o -> (RedisMessageRequest) o)
+			.map(o -> objectMapper.readValue((String) o, RedisMessageRequest.class))
 			.toList();
 	}
 
-	/**
-	 * key 생성
-	 */
 	private String buildKey(String sessionId) {
 		return SESSION_PREFIX + sessionId + MESSAGE_SUFFIX;
 	}
 
-	/**
-	 * 공통 Redis 실행
-	 */
 	private int executeOperation(Runnable operation) {
 		try {
 			operation.run();
 			return 1;
 		} catch (Exception e) {
-			System.out.println("Redis 작업 오류 :: " + e.getMessage());
+			log.error("Redis 작업 오류: " + e.getMessage());
 			return 0;
 		}
 	}
