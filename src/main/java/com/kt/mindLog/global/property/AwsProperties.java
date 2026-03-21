9a4a9999ea90cedde559211e814bda1e8cbfbc7e
jimin <jeemin065@gmail.com>
2026-03-21 10:50:11 +0900

    refactor: add AwsProperties

--- src/main/java/com/kt/mindLog/global/property/AwsProperties.java
+++ src/main/java/com/kt/mindLog/global/property/AwsProperties.java
@@ -1,4 +1,26 @@
 package com.kt.mindLog.global.property;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+
+@Getter
+@ConfigurationProperties(prefix = "cloud.aws")
 public class AwsProperties {
+	private final String region;
+	private final S3 s3;
+
+	public AwsProperties(String region, S3 s3) {
+		this.region = region;
+		this.s3 = s3;
+	}
+
+	@Getter
+	public static class S3 {
+		private final String bucket;
+
+		public S3(String bucket) {
+			this.bucket = bucket;
+		}
+	}
 }
