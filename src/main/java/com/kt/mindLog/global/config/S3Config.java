d033308f800648c8dd12fcad173db9db039a42ef
jimin <jeemin065@gmail.com>
2026-04-14 12:38:39 +0900

    refactor: add AWS access / secret key to S3Config and AwsProperties

--- src/main/java/com/kt/mindLog/global/config/S3Config.java
+++ src/main/java/com/kt/mindLog/global/config/S3Config.java
@@ -3,6 +3,8 @@ package com.kt.mindLog.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.kt.mindLog.global.property.AwsProperties;
@@ -17,8 +19,15 @@ public class S3Config {
 
 	@Bean
 	public AmazonS3 amazonS3() {
+		BasicAWSCredentials awsCredentials =
+			new BasicAWSCredentials(
+				awsProperties.getCredentials().getAccessKey(),
+				awsProperties.getCredentials().getSecretKey()
+			);
+
 		return AmazonS3ClientBuilder.standard()
 			.withRegion(awsProperties.getRegion())
+			.withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
 			.build();
 	}
 }
--- src/main/java/com/kt/mindLog/global/property/AwsProperties.java
+++ src/main/java/com/kt/mindLog/global/property/AwsProperties.java
@@ -9,10 +9,12 @@ import lombok.Getter;
 public class AwsProperties {
 	private final String region;
 	private final S3 s3;
+	private final Credentials credentials;
 
-	public AwsProperties(String region, S3 s3) {
+	public AwsProperties(String region, S3 s3, Credentials credentials) {
 		this.region = region;
 		this.s3 = s3;
+		this.credentials = credentials;
 	}
 
 	@Getter
@@ -21,6 +23,17 @@ public class AwsProperties {
 
 		public S3(String bucket) {
 			this.bucket = bucket;
+		}
+	}
+
+	@Getter
+	public static class Credentials {
+		private final String accessKey;
+		private final String secretKey;
+
+		public Credentials(String accessKey, String secretKey) {
+			this.accessKey = accessKey;
+			this.secretKey = secretKey;
 		}
 	}
 }
